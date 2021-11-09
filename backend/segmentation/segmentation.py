81a7603828661a82dfdfd0100aa3f6cb929222d3
Sebastian <sebastiantpeter@gmail.com>
2021-11-09 11:30:10 +0100

    Renaming of function parameter

--- backend/segmentation/segmentation.py
+++ backend/segmentation/segmentation.py
@@ -6,10 +6,10 @@ from torchvision.transforms import transforms
 saved_models = {}
 
 
-def perform_segmentation(image, checkpoint_path):
-    if saved_models.get(checkpoint_path) is None:
-        saved_models[checkpoint_path] = BaseModel.load_from_checkpoint(checkpoint_path)
-    model = saved_models.get(checkpoint_path)
+def perform_segmentation(image, model_checkpoint_path):
+    if saved_models.get(model_checkpoint_path) is None:
+        saved_models[model_checkpoint_path] = BaseModel.load_from_checkpoint(model_checkpoint_path)
+    model = saved_models.get(model_checkpoint_path)
     model.float()  # necessary, because pytorch expects type 'double' otherwise
     tensor = model(transforms.ToTensor()(image)[None, ...])  # pytorch expects a batch of data, so a dimension is added
     array = tensor.detach().numpy().squeeze().astype(int)  # TODO: Conversion seems to generate negative numbers
